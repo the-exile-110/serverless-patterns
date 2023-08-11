a6e6394f7e1770d5ceb2adc3b9e7ce7dded44c3c
RUI <a6504130@gmail.com>
2023-08-11 23:43:13 +0700

    add: time wait

--- daily-export-logs/app/app.py
+++ daily-export-logs/app/app.py
@@ -1,17 +1,21 @@
+import time
 import boto3
 import datetime
 
 s3 = boto3.client('s3')
 logs = boto3.client('logs')
 
-LOG_GROUP_NAMES = ['test-log-group-1', 'test-log-group-2']
-BUCKET_NAME = 'test-bucket'
+LOG_GROUP_NAMES = ['/aws/lambda/SSTBootstrap-MetadataHandlerBEE7179C-BYPpdY5bJaSF']
+BUCKET_NAME = 'qq-tmp'
+ROTATION_DAYS = 15
 
 
 def export_logs_to_s3(log_group):
-    end_time = datetime.datetime.now() - datetime.timedelta(days=90)
-    start_time = datetime.datetime(1970, 1, 1)  # UNIX epoch start time
-    safe_log_group = log_group.replace("/", "-")  # 替换 / 为 -
+    end_time = datetime.datetime.now() - datetime.timedelta(days=ROTATION_DAYS)
+    start_time = datetime.datetime(1970, 1, 1, tzinfo=datetime.timezone.utc)  # UNIX epoch start time
+    safe_log_group = log_group[1:].replace("/", "-") if log_group.startswith("/") else log_group.replace("/", "-")  # Replace slashes with dashes for S3 prefix
+
+    print(f"Exporting logs from {log_group} to S3 bucket {BUCKET_NAME} with prefix {safe_log_group}/before_{end_time.strftime('%Y-%m-%d')}")
 
     response = logs.create_export_task(
         logGroupName=log_group,
@@ -20,17 +24,32 @@ def export_logs_to_s3(log_group):
         destination=BUCKET_NAME,
         destinationPrefix=f'{safe_log_group}/before_{end_time.strftime("%Y-%m-%d")}'
     )
-    return response['taskId']
+
+    task_id = response['taskId']
+
+    # Wait for export task to complete
+    while True:
+        response = logs.describe_export_tasks(taskId=task_id)
+        status = response['exportTasks'][0]['status']['code']
+        if status == 'COMPLETED':
+            break
+        elif status == 'FAILED':
+            print(f"Export task {task_id} failed!")
+            return
+        time.sleep(10)  # Wait for 10 seconds before checking again
+
+    return task_id
 
 
 def delete_logs(log_group):
-    ninety_days_ago_timestamp = int((datetime.datetime.now() - datetime.timedelta(days=90)).timestamp() * 1000)
+    ninety_days_ago_timestamp = int((datetime.datetime.now() - datetime.timedelta(days=ROTATION_DAYS)).timestamp() * 1000)
     streams = logs.describe_log_streams(logGroupName=log_group)
 
     for stream in streams['logStreams']:
-        # 删除那些最后的事件发生在90天前的日志流
+        # Delete the log stream if it has not been updated in the last 90 days
         if 'lastEventTimestamp' in stream and stream['lastEventTimestamp'] <= ninety_days_ago_timestamp:
             logs.delete_log_stream(logGroupName=log_group, logStreamName=stream['logStreamName'])
+            print(f"Deleted log stream {stream['logStreamName']} in log group {log_group}")
 
 
 def lambda_handler(event, context):
